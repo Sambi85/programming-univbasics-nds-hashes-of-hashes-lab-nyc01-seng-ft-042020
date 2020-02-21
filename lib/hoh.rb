65456d8634483d973a7f73e4f8f1692d2606aef8
Samuel Lesser <ineedmoreplates@gmail.com>
2020-02-21 00:11:56 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -46,15 +46,15 @@ biolife = {
   :sub_category {
   :label => "Phylum",
      :sub_category => {
-       :label => "Class"
+       :label => "Class",
           :sub_category {
             :label => "Order",
               :sub_category => {
-                :label => "Family"
+                :label => "Family",
                   :sub_category {
                     :label => "Genus",
                       :sub_category => {
-                        :label => "Species"
+                        :label => "Species",
                           :sub_category => nil}
           }
         }
